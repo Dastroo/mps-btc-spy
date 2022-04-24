825fb21be9d1654c9f5b4ea465779993abf9804d
Dastroo <dreslerdawid01@gmail.com>
2022-04-24 10:57:47 +0200

    nothing

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ if [ "$(whoami)" != root ]; then
 fi
 
 service_name="mps-btc-spy.service"
-cp -v $service_name /etc/systemd/system/$service_name
 
 # make readonly dir
 mkdir -p /usr/local/share/multi-purpose-server
 
 # launch service
+cp -v $service_name /etc/systemd/system/$service_name
 systemctl daemon-reload || echo cant reload daemon
 systemctl enable --now $service_name || echo cant enable service: $service_name
